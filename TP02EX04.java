0ab48559d7e68cc542db98dc1b2013c7b8d91311
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-05-03 18:28:46 -0300

    Create TP02EX04.java

---
+++ TP02EX04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TP02EX04 {
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+
+		int array[][] = new int[2][3]; 
+		
+for(int j=0;j<2;j++) {
+	for(int i=0; i<3; i++) {
+		array[j][i] = read.nextInt();
+	}
+		}
+
+for(int j=0;j<2;j++) {
+	for(int i=0; i<3; i++) {
+		System.out.print(array[j][i]);
+		System.out.print(" ");
+	}
+	System.out.println();
+		}
+
+	}
+}
